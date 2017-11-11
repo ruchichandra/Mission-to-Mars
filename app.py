380ccbf775e1b7cae4fcdba70c62dd27dcbd418b
unknown <ruchichandra@yahoo.com>
2017-11-11 12:52:12 -0500

    app.py update

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template, jsonify, redirect
 import pymongo
 from pymongo import MongoClient
-from mLabCredentials import username, password
 import scrape_mars
 
 # Flask setup
